1b01d93f6a704bca267abcfe68341a683ffc0e9b
Juha Paananen <juha.paananen@reaktor.fi>
2011-08-26 16:00:06 +0300

    remove unnecessary code

--- 2.js
+++ 2.js
@@ -1,12 +1,4 @@
 $(function() {
-  var allKeyUps = $(document).toObservable("keyup")
-  var allKeyDowns = $(document).toObservable("keydown")
-  
-  function always(value) { return function(_) { return value } }
-  function keyCodeIs(keyCode) { return function(event) { return event.keyCode == keyCode} }
-  function keyUps(keyCode) { return allKeyUps.Where(keyCodeIs(keyCode)) }
-  function keyDowns(keyCode) { return allKeyDowns.Where(keyCodeIs(keyCode)) }
-
   function keyState(keyCode, value) {
     return Rx.Observable.FromArray([[]])
       .Concat(keyDowns(keyCode).Select(always([value])))
