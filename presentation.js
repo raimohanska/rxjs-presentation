1bd2af0770ba9c2e1bb5384492bc1f8541b0c4a7
Juha Paananen <juha.paananen@reaktor.fi>
2011-08-29 17:02:28 +0300

    rename

--- presentation.js
+++ presentation.js
@@ -1,4 +1,4 @@
-var slides = ['intro.html', '0.html', '1.html', '2.html', '3.html', '4.html', '5.html', '6.html', '7.html']
+var slides = ['intro.html', '0.html', '1.html', '2.html', '3.html', '4.html', '5.html', '6.html', 'presentation.html']
 
 function initPresentation(id) { 
   showSrc(id + ".js") 
